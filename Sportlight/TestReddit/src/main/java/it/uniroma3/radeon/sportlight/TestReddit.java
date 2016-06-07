c240e1958d62630840103bc2e649ecd5fa2ff00d
Felice Sallustio <fel.sallustio@gmail.com>
2016-06-07 17:23:40 +0200

    Da testare bootstrap dei commenti.

--- Sportlight/TestReddit/src/main/java/it/uniroma3/radeon/sportlight/TestReddit.java
+++ Sportlight/TestReddit/src/main/java/it/uniroma3/radeon/sportlight/TestReddit.java
@@ -7,6 +7,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -15,8 +17,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestReddit {
 	private static final String REDDIT_URL_TEMPLATE = "https://www.reddit.com/r/Euro2016/.json?sort=new&raw_json=1";
+	private static final String REDDIT_URL_POST_TEMPLATE = "https://www.reddit.com/r/Euro2016/comments/%s/new.json?sort=new&raw_json=1";
+	
+	private static final String MODIFIED_USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0";
+	
+	private List<String> postIds; //in futuro questa lista verrà letta da MongoDB
 	
 	public void bootstrap() {
+		this.bootPosts();
+		this.bootComments();
+		this.postIds = new LinkedList<String>();
+	}
+	
+	private void bootPosts() {
 		Calendar cal = Calendar.getInstance();
 		Date today = cal.getTime();
 		cal.add(Calendar.YEAR, -1); //prendo la data dell'anno precedente
@@ -32,7 +45,7 @@ public class TestReddit {
 				URLConnection conn = url.openConnection();
 				
 				//workaround per l'errore HTTP 429 (Too Many Requests)
-				conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0");
+				conn.setRequestProperty("User-Agent", MODIFIED_USER_AGENT);
 				
 				ObjectMapper mapper = new ObjectMapper();
 				
@@ -46,6 +59,7 @@ public class TestReddit {
 				
 				for (JsonNode jsonChild : jsonChildren) { 
 					JsonNode jsonChildData = jsonChild.get("data");
+					this.postIds.add(jsonChildData.get("id").asText());
 					long createTime = jsonChildData.get("created").asLong()*1000;
 					
 					//se il post è stato pubblicato un'anno fa, allora blocca entrambi i cicli
@@ -77,7 +91,64 @@ public class TestReddit {
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} //attendi 10 secondi
+			}
 		} while (toIterate);
+	}
+	
+	private void bootComments() {
+		for (String postId : this.postIds) {
+			URL url = null;
+			String postUrl = String.format(REDDIT_URL_POST_TEMPLATE, postId);
+			
+			try {
+				url = new URL(postUrl);
+				URLConnection conn = url.openConnection();
+				
+				//workaround per l'errore HTTP 429 (Too Many Requests)
+				conn.setRequestProperty("User-Agent", MODIFIED_USER_AGENT);
+				
+				ObjectMapper mapper = new ObjectMapper();
+				
+				System.out.println("\nGetting Reddit data from "+url.toURI());
+				
+				JsonNode jsonRoot = mapper.readTree(conn.getInputStream());
+				
+				JsonNode jsonPost = jsonRoot.get(0).get("data");
+				JsonNode jsonComment = jsonRoot.get(1).get("data");
+				
+				JsonNode jsonPostChildren = jsonPost.get("children");
+				JsonNode jsonPostChildrenData = jsonPost.get(0).get("data");
+				
+				String selftext = jsonPostChildrenData.get("selftext").asText();
+				
+				if (!selftext.equals("")) {
+					System.out.println("Selftext: "+selftext);
+				}
+				
+				JsonNode jsonCommentChildren = jsonPost.get("children");
+				
+				for (JsonNode jsonCommentChild : jsonCommentChildren) {
+					JsonNode jsonCommentChildData = jsonCommentChild.get("data");
+					String bodyComment = jsonCommentChildData.get("body").asText();
+					System.out.println("Comment :"+bodyComment);
+				}
+				
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(1000); //attendi per un secondo (per evitare eventuali blocchi)
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }

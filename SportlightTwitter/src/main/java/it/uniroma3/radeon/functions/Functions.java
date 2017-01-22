0f1ddffcc9caa01c1f4a89d4a6a0074e51950a01
Felice Sallustio <fel.sallustio@gmail.com>
2017-01-22 10:41:41 +0100

    Cleaning some useless folders

--- SportlightTwitter/src/main/java/it/uniroma3/radeon/functions/Functions.java
+++
@@ -1,8 +0,0 @@
-package it.uniroma3.radeon.functions;
-
-public class Functions {
-	
-	public static MatchFunction match(Integer minLength, Integer maxLength, String pattern) {
-		return new MatchFunction(minLength, maxLength, pattern);
-	}
-}
